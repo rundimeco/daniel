01c3fe87b99be42bd49abcd082d9f2a1a5a58487
Lejeune Gaël <rundimeco@gmail.com>
2016-12-06 09:42:27 +0100

    process_corpus

---
+++ process_corpus.py
@@ -0,0 +1,33 @@
+from daniel import process
+import glob
+import sys
+import json
+
+def translate_justext():
+#TODO: add json ressource
+  dic= {"id": "Indonesian"}
+  return dic
+
+if len(sys.argv)!=2:
+  print "="*20
+  print "USAGE : python process_corpus.py MYjson_file.json"
+  print "for information about the format for daniel, see README.txt"
+  print "="*20
+else:
+  path_json = sys.argv[1]
+  try:
+    json = json.load(open(path_json))
+  except:
+    print "!"*20
+    print "%s is not a valid json, see README.txt for more details"%path_json
+    print "!"*20
+    exit()
+  dic_lg = translate_justext()
+  for id_file, infos in json.iteritems():
+    lg = "unknown"
+    if "language" in infos:
+      lg_iso = infos["language"]
+      if lg_iso in dic_lg:
+        lg = dic_lg[lg_iso]
+    res = process(lg, infos["path"])
+    print id_file, res
